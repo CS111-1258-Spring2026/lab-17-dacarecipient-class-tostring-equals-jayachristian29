8662185f6c47adf57486ba29f0dede29ca544bbb
jayachristian29 <jayachristianlopez@gmail.com>
2026-04-22 14:23:04 -0700

    Instantiate DACArecipient and set attributes

--- Main.java
+++ Main.java
@@ -8,6 +8,10 @@ class Main
 {
   public static void main(String[] args) 
 	{
+		DACArecipient person1 = new DACArecipient();
+
+      person1.setAll("Walker", "Marry", "12-3-456", "El Salvador", 2451564, 3956753, 3956840, 'f');
+      System.out.println(person1);
     
   }
 }
